c2f334714dba055190ee3fb8eafb8d14328ac867
miroWeb <mirodiljs2004@gmail.com>
2023-09-05 14:15:04 +0500

    KeyboardEvend

---
+++ app.js
@@ -0,0 +1,29 @@
+// const signupForm = document.getElementById("signupForm");
+// const message = document.querySelector(".message");
+// const username = document.getElementById("username");
+// const regEX = /^[a-zA-Z0-9]{6,12}$/;
+
+// signupForm.addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   const inputVal = signupForm.username.value;
+
+//   if (regEX.test(inputVal)) {
+//     message.textContent = "ma'lumotlar to'g'ri ✅";
+//   } else {
+//     message.textContent = "xato ❌";
+//   }
+// });
+
+// signupForm.username.addEventListener("keyup", (e) => {
+//   if (regEX.test(e.target.value)) {
+//     signupForm.username.setAttribute("class", "success");
+//   } else {
+//     signupForm.username.setAttribute("class", "error");
+//   }
+// });
+
+// signupForm.username.addEventListener('keyup', (e) => {
+//   if (regEX.test(e.target.value)) {
+    
+//   }
+// })
